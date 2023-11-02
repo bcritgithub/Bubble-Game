e1b9a3a00884a0d2375d14b8f588610518ee8606
bcritgithub <98739235+bcritgithub@users.noreply.github.com>
2023-11-03 00:42:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+var timer = 60;
+var score = 0;
+var hitrn=0;
+
+
+function increaseScore() {
+    score+=10;
+    document.querySelector("#scoreval").textContent = score;
+}
+
+function getNewHit() {
+    hitrn = Math.floor(Math.random()*10);
+    document.querySelector("#hitval").textContent = hitrn;
+}
+
+function makeBubble() {
+var clutter = "";
+
+for(var i=1; i<=136; i++) {
+     var rn = Math.floor(Math.random()*10)
+    clutter += `<div class="bubble">${rn}</div>`;
+}
+
+document.querySelector("#p-bottam").innerHTML = clutter;
+}
+
+function runTimer() {
+    var timerint = setInterval(function(){
+        if(timer>0){
+            timer --;
+            document.querySelector("#timerval").textContent = timer;
+        }
+        else {
+            clearInterval(timerint);
+            document.querySelector("p-bottam").innerHTML = `<h1>Game Over</h1>`;
+        }
+    } , 1000);
+
+}
+
+document.querySelector("#p-bottam").addEventListener("click",function(details){
+    var clickednum = Number(details.target.textContent);
+     if(clickednum === hitrn) {
+        increaseScore();
+        makeBubble();
+        getNewHit();
+     }
+});
+
+runTimer();
+makeBubble();
+getNewHit();
+
